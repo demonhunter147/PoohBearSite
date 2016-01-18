d2a520ae043fffe82455927acea5b1952863be08
Michanette <Michanette>
2016-01-18 08:11:19 +0200

    did stuff

--- PoohBear/WebApplication1/Controllers/HomeController.cs
+++ PoohBear/WebApplication1/Controllers/HomeController.cs
@@ -3,6 +3,10 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.Net.Mail;
+using WebApplication1.Models;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
 
 namespace WebApplication1.Controllers
 {
@@ -13,11 +17,47 @@ namespace WebApplication1.Controllers
             return View();
         }
 
-        public ActionResult Contact()
+        [HttpPost]
+        public ActionResult Index(HomeModels c)
         {
-            ViewBag.Message = "Your contact page.";
-
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    MailMessage msg = new MailMessage();
+                    SmtpClient smtp = new SmtpClient();
+                    MailAddress from = new MailAddress(c.Email.ToString());
+                    StringBuilder sb = new StringBuilder();
+                    msg.To.Add("poohbearandfriendz@gmail.com");
+                    msg.Subject = "Contact Us";
+                    msg.IsBodyHtml = false;
+                    SmtpClient client = new SmtpClient();
+                    // We use gmail as our smtp client
+                    client.Host = "smtp.gmail.com";
+                    client.Port = 587;
+                    client.EnableSsl = true;
+                    client.DeliveryMethod = SmtpDeliveryMethod.Network;
+                    client.Credentials = new System.Net.NetworkCredential("poohbearandfriendz@gmail.com", "Believe123");
+                    sb.Append("Name: " + c.Name);
+                    sb.Append(Environment.NewLine);
+                    sb.Append("Phione Number: " + c.Phone);
+                    sb.Append(Environment.NewLine);
+                    sb.Append("Email: " + c.Email);
+                    sb.Append(Environment.NewLine);
+                    sb.Append("Message: " + c.Message);
+                    msg.Body = sb.ToString();
+                    smtp.Send(msg);
+                    msg.Dispose();
+                    return View("Success");
+                }
+                catch (Exception)
+                {
+                    return View("Error");
+                }
+            }
             return View();
         }
     }
 }
+
+
---
+++ PoohBear/WebApplication1/Models/HomeModels.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace WebApplication1.Models
+{
+    public class HomeModels
+    {
+
+        [Required(ErrorMessage = "Your Name is required")]
+        public string Name { get; set; }
+
+        public string Phone { get; set; }
+
+        [Required(ErrorMessage = "Your Email address is required")]
+        public string Email { get; set; }
+
+        [Required(ErrorMessage = "Please type your message")]
+        public string Message { get; set; }
+    }
+}
